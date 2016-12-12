be6d37a6dc5c1e139a77f542dd2866952bfe2a5d
chantanawat chasaeng <chantanawat@hotmail.co.th>
2016-12-12 20:21:05 +0700

    Add Entity project

---
+++ src/main/java/com/roo/test/domain/Gold.java
@@ -0,0 +1,18 @@
+package com.roo.test.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Gold {
+
+    /**
+     */
+    @NotNull
+    @Size(min = 2)
+    private String Name;
+}
